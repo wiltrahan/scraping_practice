9e97521b93365ca435e12bd2ebfb351b207e0a97
wiltrahan <twilorip@gmail.com>
2018-02-07 17:42:57 -0500

    login working

---
+++ WebDriver_basics/src/CleanScrape.java
@@ -0,0 +1,50 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class CleanScrape {
+
+    public WebDriver driver = new FirefoxDriver();
+
+    public void openSite() {
+        driver.get("https://www.yahoo.com/");
+
+        try {
+            driver.findElement(By.id("uh-signin")).click();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void login(String username, String password) {
+        try {
+            driver.findElement(By.id("login-username")).sendKeys(username);
+            driver.findElement(By.id("login-signin")).click();
+            pass(password);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public void pass(String password) {
+        try {
+            driver.findElement(By.id("login-passwd")).sendKeys(password);
+            driver.findElement(By.id("login-signin")).click();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Personal personal = new Personal();
+        String username = personal.getUsername();
+        String password = personal.getPassword();
+
+        System.setProperty("webdriver.gecko.driver", "/Users/twilorip/Desktop/webDrivers/firefox/geckodriver");
+        CleanScrape cleanScrape = new CleanScrape();
+        cleanScrape.openSite();
+        cleanScrape.login(username, password);
+
+    }
+}
