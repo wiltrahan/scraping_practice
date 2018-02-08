638e5285410c46b27ddf456f28666fda39b47797
wiltrahan <twilorip@gmail.com>
2018-02-08 18:50:14 -0500

    shorten login process

--- WebDriver_basics/src/StartHere.java
+++ WebDriver_basics/src/StartHere.java
@@ -14,14 +14,15 @@ public class StartHere {
         System.setProperty("webdriver.gecko.driver", "/Users/twilorip/Desktop/webDrivers/firefox/geckodriver");
         StartHere startHere = new StartHere();
         startHere.openSite();
-        startHere.login(username, password);
+//        startHere.login(username, password);
+
     }
 
     private void openSite() {
-        driver.get("https://www.yahoo.com/");
+        driver.get("https://finance.yahoo.com/portfolios");
 
         try {
-            driver.findElement(By.id("uh-signin")).click();
+            driver.findElement(By.cssSelector("._299Wd")).click();
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -50,5 +51,24 @@ public class StartHere {
 
     private void openFinance() {
         driver.navigate().to("https://finance.yahoo.com/");
+        driver.findElement(By.xpath("//a[@class='_299Wd']")).click();
+
     }
+
+//    private void goToPortfolio() {
+//        //locate the menu to hover over using its xpath
+//
+//        WebElement menu = driver.findElement(By.xpath("//*[@id=\"Nav-0-DesktopNav\"]/div/div[3]/div/div[1]/ul/li[2]/a/text()")).click();
+//
+//        //Initiate mouse action using Actions class
+//        Actions builder = new Actions(driver);
+//
+//        // move the mouse to the earlier identified menu option
+//        builder.moveToElement(menu).build().perform();
+//
+//        // wait for max of 5 seconds before proceeding. This allows sub menu appears properly before trying to click on it
+//        WebDriverWait wait = new WebDriverWait(driver, 5);
+//        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='subNav_music_menu']/tbody/tr[2]/td[1]/a[1]")));  // until this submenu is found
+//    }
 }
+//*[@id="Nav-0-DesktopNav"]/div/div[3]/div/div[1]/ul/li[2]/a/text()
