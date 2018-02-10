4ffbafbe538fdd41089047cc0214da0f1911fd37
wiltrahan <twilorip@gmail.com>
2018-02-10 17:18:47 -0500

    connect db

---
+++ WebDriver_basics/src/DB.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DB {
+
+    public static void main(String[] args) {
+
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:/Users/twilorip/Desktop/scraping_practice/WebDriver_basics/portfolio.db");
+            Statement statement = conn.createStatement();
+            statement.execute("CREATE TABLE IF NOT EXISTS portfolio (symbol TEXT, value INTEGER, change INTEGER, shares INTEGER)");
+
+            statement.close();
+            conn.close();
+
+        } catch(SQLException e) {
+            System.out.println("Something went wrong " + e.getMessage());
+        }
+
+    }
+}
